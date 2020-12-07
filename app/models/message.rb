80dc57a5180dec1d086812abf123ba7687a84fba
pwkurylo <43602712+pwkurylo@users.noreply.github.com>
2020-12-07 12:54:36 +0200

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
 
   validates :message, :uniq_key, presence: true
   validates :uniq_key, uniqueness: true
-  validates_length_of :message, :maximum => 1500
+  validates_length_of :message, :maximum => 2000
 
   before_create :encrypt_message
 
